f0d2b599ff7ffeeec929b902129a0d8ac230467a
Abel Roinsard <abelroinsard@gmail.com>
2024-12-03 00:09:29 +0100

    refactor: linting

--- src/components/listActions.tsx
+++ src/components/listActions.tsx
@@ -2,26 +2,23 @@ import { Action, ActionPanel, Icon } from "@raycast/api";
 import PostDetails from "./postDetails";
 import { PostDetailsProps } from "../types/types";
 
-function ListActions(props: PostDetailsProps ) {
-    return (
-        <ActionPanel title={`Post ID: ${props.post.id}`}>
-            <Action.OpenInBrowser title="Open in Danbooru" url={`https://danbooru.donmai.us/posts/${props.post.id}`} />
-            <Action.Push title="See Details" target={<PostDetails post={props.post} />} icon={Icon.Document} />
-            <Action.OpenInBrowser title="Open large view" url={props.post.file_url} />
-        </ActionPanel>
-    );
+function ListActions(props: PostDetailsProps) {
+  return (
+    <ActionPanel title={`Post ID: ${props.post.id}`}>
+      <Action.OpenInBrowser title="Open in Danbooru" url={`https://danbooru.donmai.us/posts/${props.post.id}`} />
+      <Action.Push title="See Details" target={<PostDetails post={props.post} />} icon={Icon.Document} />
+      <Action.OpenInBrowser title="Open Large View" url={props.post.file_url} />
+    </ActionPanel>
+  );
 }
 
-function DetailActions(props: PostDetailsProps ) {
-    return (
-        <ActionPanel title={`Post ID: ${props.post.id}`}>
-            <Action.OpenInBrowser title="Open in Danbooru" url={`https://danbooru.donmai.us/posts/${props.post.id}`} />
-            <Action.OpenInBrowser title="Open large view" url={props.post.file_url} />
-        </ActionPanel>
-    );
+function DetailActions(props: PostDetailsProps) {
+  return (
+    <ActionPanel title={`Post ID: ${props.post.id}`}>
+      <Action.OpenInBrowser title="Open in Danbooru" url={`https://danbooru.donmai.us/posts/${props.post.id}`} />
+      <Action.OpenInBrowser title="Open Large View" url={props.post.file_url} />
+    </ActionPanel>
+  );
 }
 
-export {
-    ListActions,
-    DetailActions
-}
+export { ListActions, DetailActions };
